0b62e086d47571c5c5044a88ecd13423ae1e5eb8
Charlie <Charlie@DESKTOP-1UD2VPV>
2022-01-29 18:42:25 +0000

    Add project files.

---
+++ MyFirstGame/Program.cs
@@ -0,0 +1,35 @@
+﻿const int numQuestions = 10;
+const int minNumber = -10;
+const int maxNumber = 10;
+
+var rng = new Random(DateTime.Now.Millisecond);
+var score = 0;
+
+Console.WriteLine("Welcome to my first game!");
+Console.WriteLine();
+
+for (var i = 1; i <= numQuestions; i++)
+{
+    var a = rng.Next(minNumber, maxNumber);
+    var b = rng.Next(minNumber, maxNumber);
+
+    Console.WriteLine();
+    Console.WriteLine($"Question {i}) What is: {a} + {b}");
+
+    Console.WriteLine();
+    Console.Write("What's your answer? ");
+
+    if (int.TryParse(Console.ReadLine(), out int result) && result == a + b)
+    {
+        Console.WriteLine("Correct!");
+        score++;
+    }
+    else
+    {
+        Console.WriteLine("Wrong!");
+    }
+}
+
+Console.WriteLine();
+Console.WriteLine($"Your final score is {score} out of {numQuestions}");
+Console.WriteLine();
